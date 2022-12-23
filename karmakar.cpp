5591f06cf3cd9040d074f865e1afcf1e54b8c0d2
Stefano Tumino <47145720+Nefasto1@users.noreply.github.com>
2022-12-23 12:20:41 +0100

    Aggiunta struttura karmakar
    Codice completamente commentato contenente la struttura template di karmakar, contiene costruttore vuoto, algoritmo di karmakar con ottimizzazione selezionabile

--- karmakar.cpp
+++ karmakar.cpp
@@ -7,60 +7,266 @@
 
 using namespace boost::numeric::ublas;
 
-enum class SolutionType
-{
-    BOUNDED,  // trovata una soluzione ottima
-    UNBOUNDED // l'insieme delle soluzioni non è limitato superiormente
+/**
+ * @brief Classe template che rappresenta il sistema lineare di vincoli per un problema di programmazione lineare 
+ *  
+ * @tparam T: Tipo dei coefficienti del sistema lineare
+*/
+template<typename T>
+struct LinearConstrainSystem {
+    matrix<T> A;        //!< Matrice dei coefficienti dei vincoli
+    vector<T> b;        //!< Condizioni dei vincoli
+    uint k;             //!< Numero di ripetizioni
+
+    /**
+     * @brief Classe di enumerazione per definire se il sistema ha una soluzione ottima o meno
+    */
+    enum class SolutionType
+    {
+        BOUNDED,  //!< Indica che il sistema ha una soluzione ottima 
+        UNBOUNDED //!< Indica che il sistema NON ha una soluzione ottima 
+    };
+
+    /**
+     * @brief Classe di enumerazione per definire se il sistema è utilizzato per un problema di massimizzazione o minimizzazione
+    */
+    enum class OptimizationType { 
+        MIN, //!< Indica che il problema è di minimizzazione
+        MAX  //!< Indica che il problema è di massimizzazione
+    };
+
+    LinearConstrainSystem()
+        : A{nullptr}, b{nullptr}, k{0}
+    {}
+    
+    LinearConstrainSystem(matrix<T> A, vector<T> b)
+        : A{A}, b{b}, k{0}
+    {}
+
+    /**
+     * @brief Metodo che crea una matrice diagonale con gli elementi del vettore passato in input
+     * 
+     * @param vector: Vettore da inserire come diagonale principale
+     * @return m: Matrice diagonale
+    */
+    matrix<T> diagonale(vector<T> &vector){
+        // Creo una matrice quadrata di dimensione pari a quella del vettore in input con tutti gli elementi posti a zero
+        matrix<T> m{zero_matrix<T>(vector.size(), vector.size())};
+
+        // Inserisco nella diagonale principale il gli elementi del vettore
+        for (size_t i{0}; i < vector.size(); ++i)
+        {
+            m(i, i) = vector(i);
+        }
+
+        return m;
+    }
+
+    /**
+     * @brief Metodo che inverte la matrice passata in input
+     * 
+     * @param input: Matrice da invertire
+     * @param inverse: Matrice dove inserire l'inversa
+     * @return booleano che indica se la matrice è invertibile o meno
+    */
+    bool invertMatrix(const matrix<T> &input, matrix<T> &inverse){
+        // Definisco una matrice di permutazione
+        typedef permutation_matrix<std::size_t> pmatrix;
+        // Inizializzo una matrice di lavoro
+        matrix<T> A(input);
+        // Crea una matrice di permutazione per la fattorizzazione LU
+        pmatrix pm(A.size1());     
+        // Esegue la fattorizzazione LU          
+        const int res = lu_factorize(A, pm); 
+        // Se la fattorizzazione è nulla allora non è invertibile
+        if (res != 0)
+        {
+            return false;
+        }
+        // Assegnamo alla matrice inversa il valore della matrice identità
+        inverse.assign(identity_matrix<T>(A.size1()));
+        // Inverto la matrice
+        lu_substitute(A, pm, inverse);
+        return true;
+    }
+
+    /**
+     * @brief Metodo che calcola la differenza fra due array
+     * 
+     * @param a: primo vettore
+     * @param b: secondo vettore
+     * @return difference: La differenza tra i due array
+    */
+    T diff(vector<T> &a, vector<T> &b){
+        // Se la dimensione dei due array differisce usciamo
+        if (a.size() != b.size()){
+                std::cout << "Array di dimensione diversa, uscita..." << std::endl;
+                exit(1);
+        }
+        
+        // Calcolo la difference come la somma dei valori assoluti della differenza dei due array
+        T difference{0};
+        for(uint i{0}; i < a.size(); ++i){
+            difference += std::abs(a[i]-b[i]);
+        }
+
+        return difference;
+    }
+
+    /**
+     * @brief Metodo che controlla se un problema ha soluzione ottimale o meno
+     * 
+     * @param hv: Array dal quale effettuare il controllo
+     * @param opt: Tipo di ottimizzazione selezionato
+     * @return unbounded: Booleano che indica se il sistema NON ha soluzione
+    */
+    bool isUnbounded(vector<T> hv, const OptimizationType &opt){
+            bool unbounded{true};
+
+            // Controllo se tutti gli elementi...
+            for (uint i{0}; i < hv.size(); ++i){
+                // ...Sono negativi nel caso della massimizzazione
+                if (hv(i) < 0 && opt==OptimizationType::MAX){
+                    unbounded = false;
+                }
+                // ...Sono positivi nel caso della minimizzazione
+                else if (hv(i) > 0 && opt==OptimizationType::MIN){
+                    unbounded = false;
+                }
+            }
+
+            return unbounded;
+    }
+
+    /**
+     * @brief Calcolo la direzione da seguire per il nuovo punto ottimale
+     * 
+     * @param v: array v
+     * @param hv: array hv
+     * @param opt: Tipo di soluzione selezionata
+     * @return dir: la direzione da seguire
+    */
+    T direction(vector<T> v, vector<T> hv, const OptimizationType &opt){
+        T dir;
+        bool first{true};
+
+        // Per ogni elemento dei vettori...
+        for (uint i{0}; i < hv.size(); ++i){
+            // ...Se il valore di hv è negativo e stiamo massimizzando...
+            if (hv(i) < 0 && opt==OptimizationType::MAX){
+                T tmp = -v(i) / hv(i);
+                // ...Ed il valore trovato è il primo o è minore di quello precedente lo salviamo
+                if (dir > tmp || first){
+                    if (first){
+                        first = false;
+                    }
+                    dir = tmp;
+                } 
+            }
+            // ...Altrimenti se il valore di hv è positivo e stiamo minimizzando...
+            else if (hv(i) > 0 && opt==OptimizationType::MIN){
+                T tmp = -v(i) / hv(i);
+                // ...Ed il valore trovato è il primo o è maggiore di quello precedente lo salviamo
+                if (dir < tmp || first){
+                    if (first){
+                        first = false;
+                    }
+                    dir = tmp;
+                } 
+            }
+        }
+
+        return dir;
+    }
+
+    /**
+     * @brief Metodo che effettua l'algoritmo di karmakar
+     * 
+     * @tparam STOP_CRITERION: criterio di Stop selezionato
+     * @param solution: Array che conterrà la soluzione
+     * @param c: Array contenente i costi del sistema
+     * @param gamma: Step-size
+     * @param type: Tipo di ottimizzazione scelto
+     * @return Se il sistema ammette una soluzione ottima o meno
+    */
+    template <typename STOP_CRITERION>
+    SolutionType karmakar(vector<T>& solution, const vector<T>& c, const T gamma, const OptimizationType type, uint repetitions){
+        vector<T> xprev{solution*2};
+        
+        while (((this->k) < repetitions-1) && (diff(xprev, solution) > 1e-16)){
+            ++(this->k);
+            
+            // Calcolo il vettore v che contiene la differenza tra l'array b ed il prodotto della matrice A con il vettore dei punti x
+            vector<T> v((this->b).size());
+            v = (this->b) - prod((this->A), solution) + vector<T>((this->b).size(), 1e-16);
+            std::cout << "v[" << (this->k) << "] = " << v << " ____ "
+                    << "x[" << (this->k) << "] = " << solution << " ____ " << std::endl;
+
+            // Calcolo la matrice diagonale Dv
+            matrix<T> Dv((this->b).size(), (this->b).size());
+            Dv = diagonale(v);
+            // Se non è invertibile usciamo
+            matrix<T> Dv_inversa((this->b).size(), (this->b).size());
+            if (!invertMatrix(Dv, Dv_inversa))
+            {
+                std::cout << "Matrice Dv non invertibile" << std::endl;
+                exit(1);
+            }
+
+            matrix<T> m((this->b).size(), (this->b).size());
+            matrix<T> mInv((this->b).size(), (this->b).size());
+            // Calcoliamo il quadrato della matrice inversa di Dv
+            m = prod(Dv_inversa, Dv_inversa);
+            // Effettuo il prodotto tra la matrice A trasposta e Dv^-2
+            m = prod(trans((this->A)), m);
+            // Effettuo il prodotto tra la matrice T(A)Dv^-2 e A
+            m = prod(m, (this->A));
+            // Se non è invertibile usciamo
+            if (!invertMatrix(m, mInv))
+            {
+                std::cout << "Matrice T(A)Dv^{-2}A non invertibile" << std::endl;
+                exit(1);
+            }
+
+            // Effettuiamo il prodotto tra la matrice inversa di T(A)Dv^{-2}A e c
+            vector<T> hx((this->b).size());
+            hx = prod(mInv, c);
+            // Effettuiamo il prodotto tra la matrice A e la matrice hx
+            vector<T> hv((this->b).size());
+            hv = prod(-(this->A), hx);
+            // Se il sistema non ammette una soluzione ottima usciamo
+            if (isUnbounded(hv, type))
+            {
+                return SolutionType::UNBOUNDED;
+            }
+
+            // Calcoliamo la direzione da seguire per aggiornare la soluzione ottimale
+            T dir = direction(v, hv, type);
+            
+            // Calcoliamo la nuova soluzione ottimale salvando quella precedente
+            xprev = solution;
+            solution = solution + dir * gamma * hx;
+        }
+        
+        // Se arriviamo a questo punto abbiamo trovato una soluzione ottimale
+        T res{0};
+        for (uint i{0}; i < c.size(); ++i)
+        {
+            res += c(i) * solution(i);
+        }
+        std::cout << "Il massimo è: " << res << std::endl;
+
+        return SolutionType::BOUNDED;
+    }
 };
-
-/**
- * Funzione per ottenere una matrice diagonale da un vettore
- *
- * La matrice diagonale avrà in posizione (i, i) il valore vector(i)
- *
- * @param vector è il vettore i cui valori devono formare la diagonale della matrice
- * @return è la matrica diagonale
- */
-matrix<double> diagonale(vector<double> &vector);
-
-/**
- * Funzione per ottenere il quadrato dell'inversa di una matrice
- *
- * @param input è la matrice su cui si vuole compiere l'inversione
- * @param inverse è la matrice inversa
- * @return è una condizione di verità che è vera se la matrice è invertibile
- */
-bool invertMatrix(const matrix<double> &input, matrix<double> &inverse);
-
-/**
- * Versione del algoritmo di Karmakar con l'utilizzo delle trasformazioni affini
- * Non è un algoritmo di complessità polinomiale
- *
- * @param A matrice di Ax <= b
- * @param b
- * @param c vettore di cTx (cT è il vettore riga di c)
- * @param x0
- * @param repetitions è il numero di iterazioni consentite per ottenere il risultato
- * @param gamma
- * @return è il vettore massimo
- */
-SolutionType affineScaling(
-    matrix<double> &A,
-    vector<double> &b,
-    vector<double> &c,
-    vector<double> &x0,
-    uint &repetitions,
-    double gamma
-);
 
 int main()
 {
-    using namespace boost::numeric::ublas;
 
-    /*    matrix<double> m(2, 2, 0.9);
+    /*    matrix<T> m(2, 2, 0.9);
     m(0, 1) = 0;
     m(1, 0) = 0;
-    matrix<double> inv_m(2, 2, 0);
+    matrix<T> inv_m(2, 2, 0);
     bool isInvertible{invertMatrix(m, inv_m)};
     std::cout << "m" << m << " ___ inv_m" << inv_m << std::endl;
     */
@@ -75,126 +281,22 @@ int main()
     vector<double> x0(2, 0.5);
     x0(1) = 0;
     uint repetitions{10};
-    if (affineScaling(A, b, c, x0, repetitions, 0.5) == SolutionType::BOUNDED)
-    {
-        std::cout << "BOUNDED solution" << std::endl;
+    LinearConstrainSystem<double> obj(A, b);
+    auto solution = obj.karmakar<int>(x0, c, 0.5, obj.OptimizationType::MAX, 50);
+    if (solution == obj.SolutionType::BOUNDED){
+        std::cout << "Bounded" << std::endl;
+        std::cout << "Soluzione: " << x0 << std::endl;
+    } else {
+        std::cout << "Unbounded" << std::endl;
     }
-    else
-    {
-        std::cout << "UNBOUNDED solution" << std::endl;
-    }
+    // if (affineScaling(A, b, c, x0, OptimizationType::MIN, repetitions, 0.5) == SolutionType::BOUNDED)
+    // {
+    //     std::cout << "BOUNDED solution" << std::endl;
+    // }
+    // else
+    // {
+    //     std::cout << "UNBOUNDED solution" << std::endl;
+    // }
 
     return 0;
-}
-
-matrix<double> diagonale(vector<double> &vector)
-{
-    matrix<double> m{zero_matrix<double>(vector.size(), vector.size())};
-
-    for (size_t i{0}; i < vector.size(); ++i)
-    {
-        m(i, i) = vector(i);
-    }
-
-    return m;
-}
-
-bool invertMatrix(const matrix<double> &input, matrix<double> &inverse)
-{
-    typedef permutation_matrix<std::size_t> pmatrix;
-    matrix<double> A(input);
-    pmatrix pm(A.size1());               // crea una matrice di permutazione per la fattorizzazione LU
-    const int res = lu_factorize(A, pm); // esegue la fattorizzazione LU
-    // std::cout << "res = " << res << std::endl;
-    if (res != 0)
-    {
-        return false;
-    }
-    inverse.assign(identity_matrix<double>(A.size1())); // crea la matrice identità
-    lu_substitute(A, pm, inverse);
-    return true;
-}
-
-SolutionType affineScaling(matrix<double> &A, vector<double> &b, vector<double> &c, vector<double> &x0, uint &repetitions, double gamma)
-{
-    u_int k{0};
-    vector<double> v(b.size());
-    vector<double> x{x0};
-    matrix<double> Dv(b.size(), b.size());
-    matrix<double> Dv_inversa(b.size(), b.size());
-    matrix<double> mInv(b.size(), b.size());
-    vector<double> hx(b.size());
-    vector<double> hv(b.size());
-    // double alpha;
-    double min;
-    double tmp;
-    bool isUnbounded{true};
-    bool isFirst;
-
-    while (k < repetitions)
-    {
-        v = b - prod(A, x) + vector<double>(b.size(), 1e-16);
-        std::cout << "v[" << k << "] = " << v << " ____ "
-                  << "x[" << k << "] = " << x << " ____ " << std::endl;
-        Dv = diagonale(v);
-        if (!invertMatrix(Dv, Dv_inversa))
-        {
-            throw("Matrice Dv non invertibile");
-        }
-        Dv = prod(Dv_inversa, Dv_inversa);
-        Dv = prod(trans(A), Dv);
-        Dv = prod(Dv, A);
-        if (!invertMatrix(Dv, Dv_inversa))
-        {
-            throw("Matrice A^tDv^{-2}A non invertibile");
-        }
-        hx = prod(Dv_inversa, c);
-        hv = prod(-A, hx);
-        for (uint i{0}; i < hv.size(); ++i)
-        {
-            if (hv(i) < 0)
-            {
-                isUnbounded = false;
-            }
-        }
-        if (isUnbounded)
-        {
-            return SolutionType::UNBOUNDED;
-        }
-
-        isFirst = true;
-        for (uint i{0}; i < b.size(); ++i)
-        {
-            if (hv(i) < 0)
-            {
-                if (isFirst)
-                {
-                    min = -v(i) / hv(i);
-                    isFirst = false;
-                }
-                else
-                {
-                    tmp = -v(i) / hv(i);
-                    if (min > tmp)
-                    {
-                        min = tmp;
-                    }
-                }
-            }
-        }
-        if (k < repetitions - 1)
-        {
-            x = x + min * gamma * hx;
-        }
-
-        ++k;
-    }
-    double res{0};
-    for (uint i{0}; i < c.size(); ++i)
-    {
-        res += c(i) * x(i);
-    }
-    std::cout << "Il massimo è: " << res << std::endl;
-
-    return SolutionType::BOUNDED;
 }
